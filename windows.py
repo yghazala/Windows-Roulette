6955a216b4195f5b3b8dee5fbcf92f2a1fbd2950
oSynq <yghazala@outlook.com>
2024-04-20 17:55:36 +0400

    Fixed my smart spelling error
    (still doesnt work if you do it, its a joke)

--- windows.py
+++ windows.py
@@ -2,7 +2,7 @@ import random
 import os
 
 selectednumber=int(input("Pick a number 1-10: "))
-randomnumber=random.rantint(1,10)
+randomnumber=random.randint(1,10)
 
 if selectednumber == randomnumber:
     print("You're Lucky...")
